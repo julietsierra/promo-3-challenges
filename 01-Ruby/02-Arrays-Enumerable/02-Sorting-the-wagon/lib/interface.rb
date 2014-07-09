901e166d32a7fee86cac10693f4c9a465dda3598
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-09 19:09:15 +0200

    resolved

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,25 +6,18 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
-  print name
-
+  students << name if name !=""
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+  classified_wagon = wagon_sort(students)
 
-sorted_students=wagon_sort(students)
-
-#afficher le congrats message#
-if sorted_students==1
-  congrats_message = "Congratulations, your wagon has 1 student!"
-else "congratulations, your wagon has #{student_list.size} students!"
-end
-
-puts congrats_message
+  print "Congratulations! Your Wagon has #{students.size} students :  "
+  puts classified_wagon[0..students.size-2].join(", ") + " and " + classified_wagon.last
 
 
 
 
 
 
-# TODO: Call `wagon_sort` method and display the sorted student list
+
