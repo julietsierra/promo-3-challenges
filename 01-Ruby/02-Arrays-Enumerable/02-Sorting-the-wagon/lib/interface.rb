22a5ef75c642ba30602cf2ab475cf1a2fb823d8a
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-09 17:08:07 +0200

    exo

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,25 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
+  print name
 
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+
+sorted_students=wagon_sort(students)
+
+#afficher le congrats message#
+if sorted_students==1
+  congrats_message = "Congratulations, your wagon has 1 student!"
+else "congratulations, your wagon has #{student_list.size} students!"
+end
+
+puts congrats_message
+
+
+
+
+
 
 # TODO: Call `wagon_sort` method and display the sorted student list
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,8 @@
 def wagon_sort(students)
-  # TODO: return a copy of students, sorted alphabetically
+  return students.sort_by { |name| name.upcase}
 end
+
+
+
+  # TODO: return a copy of students, sorted alphabetically
+
