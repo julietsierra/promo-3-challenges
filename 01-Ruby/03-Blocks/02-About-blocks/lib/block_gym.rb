aa31370dd94b55fd6337b84ffa6f0bbba7bb2162
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-10 14:16:12 +0200

    first attempt

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,4 +1,7 @@
 def tag(tag_name, attr = nil)
+
+  "<#{tag_name}>#{qq_chose}</#{tag_name}"
+
   #TODO:  Build HTML tags around  content given in the block
   #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
 end
