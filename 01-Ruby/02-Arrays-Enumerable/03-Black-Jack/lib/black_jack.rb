084ad611fc258e431ab6e41fff34f1c4292f3215
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-14 17:08:58 +0200

    solved git commit --msolved
    git status
    exit

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,17 +1,13 @@
 
-
-player_score=0
-bank_score = 0
-
 def bank_score
-  random.rand(16..21)
+  rand(16..21)
 end
 
 def pick_card
-  random.rand(1..11)
+  rand(1..11)
 end
 
 def game_outcome(bank, score)
- [bank, score]
   # TODO: Take the bank and the score and output an array containing the bank and then the score
+   [bank, score]
 end
