d565fc13899bad7b318ad22bd07fd55f96429e8a
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-14 16:42:31 +0200

    work in progres

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,50 +1,37 @@
-
 require_relative 'black_jack'
 
 def play_game
-  bank= bank_score
-  player_score = 0
-
-    while asking_for_card?(player_score)
-     player_score += pick_card
-     puts state_of_the_game(player_score,bank)
-end
-
-puts outcome = game_outcome(player_score,bank)
-puts build_message_for(outcome)
-end
-
-def state_of_the_game(score, bank)
-  "Your score is #{player_score}, bank i #{bank}"
-  end
-
- def asking_for_card?(answer)
-  puts "Do you want a card"
-  player_answer = gets.chomp
-  player_answer == "Y" || player_answer == "yes"
-  return true
-else return false
-
-
-
-end
+  while puts "Pick ask card"
+  reply= gets.chomp
+  if reply== "Yes" || reply== "Y"
+    true
+  pick_card
 
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+  end
+
+def state_of_the_game(score, bank)
+  puts "bank score: #{bank} your score: #{score}"
+  # TODO: Returns custom message with player's score and bank's score
 end
 
+def asking_for_card?(score)
+   if score <= 21
+    return true
+    play_game
+   else
+    build_message_for(state_of_the_game)
+   end
 
-
-# TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
-
+  # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+end
 
 def build_message_for(outcome)
-  if outcome[1] > 21
-    puts "you loose"
-  elsif outcome[1] == 21
-    puts "Black Jack !"
-  elsif outcome[0] < outcome[1]
-    puts "you win"
-  else
-    puts "you're over you loose "
+  if bank <= score
+    puts" you loose"
+  elsif score == 21
+   puts "Black Jack !"
+  else puts "you loose"
+  end
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
 end
