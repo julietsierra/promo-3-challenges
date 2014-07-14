c3d87fb9e966d3ea4488c1cfb4054b6d483a2787
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-14 17:51:50 +0200

    in progress

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,4 +1,7 @@
 def pmu_format!(race_array)
+  race_array.each_with_index do |race_array, index|
+    puts "#{index}" "#{race_array}"
+end
   #TODO: modify the given array so that it is PMU-consistent
 end
 
