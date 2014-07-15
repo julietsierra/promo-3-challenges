ce988bd23fc21a182e71d5289e01c646222b87b7
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-15 17:41:48 +0200

    work in progress

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,7 @@
+File.open("/source_text.txt", "r").each_line do |line|
+  puts line
+end
+
 def most_common_words(file_name, stop_words_file_name, number_of_word)
   #TODO: return hash of occurences of number_of_word most frequent words
 end
