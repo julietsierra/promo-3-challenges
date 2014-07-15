af47526b67bf6d7d9e0302cd1cbbba18dfb5888a
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-15 12:21:51 +0200

    solved

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,13 @@
 def french_phone_number?(phone_number)
-  # TODO: true or false?
+  french_number= phone_number.gsub(/\+33/, "0")
+  french_number= french_number.gsub(/\s|\-/, '')
+
+  if french_number =~ /^0[1-9]\d{8}$/
+    true
+  else
+    false
+  end
 end
+
+puts french_phone_number?("+33667571112")
+
