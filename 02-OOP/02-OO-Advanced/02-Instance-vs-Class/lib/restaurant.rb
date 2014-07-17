52b7a1e1628c6480a9cdbfde65e9a56f29d5ac5c
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-17 17:08:10 +0200

    ok

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,46 @@
+
 class Restaurant
-  
-  #TODO add relevant accessors if necessary
-  
-  def initialize(city, name)
-    #TODO: implement constructor with relevant instance variables
+  attr_reader :average_rating, :rating
+  attr_accessor :city, :name
+  def initialize(name,city)
+     @name = name
+     @average_rating = 0
+     @rating= []
+     @city = city
   end
-  
-  #TODO: implement #filter_by_city and #rate methods
-  
+
+  def rate(grade)
+    @rating << grade
+    @average_rating = @rating.inject(:+) / @rating.size
+
+  end
+
+
+
+
+  #TODO add relevant accessors if necessary
+def self.filter_by_city(names,city)
+  result=[]
+  names = []
+  names.each do |restaurant|
+    if restaurant.city == city
+     result << restaurant
+     else
+       false
+    end
+  end
+    return result
+  end
+
+
+    #TODO: implement constructor with relevant instance variables
 end
+
+
+
+
+
+
+  #TODO: implement #filter_by_city and #rate methods
+
+
