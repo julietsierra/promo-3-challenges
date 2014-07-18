9e96a859e1764ae2cc598d15f23f951c719a6869
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-18 15:54:18 +0200

    o

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
@@ -1,9 +1,32 @@
+require 'csv'
+
 class Cookbook
-  def initialize(file)
-    # TODO: Retrieve the data from your CSV file and store it in an instance variable
+  def initialize
+    @all_recipes = []
+    @file_path = "/Users/juliette_sanson/code/julietsierra/promo-3-challenges/02-OOP/03-Cookbook/01-Cookbook-v0/lib/recipes.csv"
   end
 
-  # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
-  # TODO: Implement a save method that will write the data into the CSV
-  # And don't forget to use this save method when you have to modify something in your recipes array.
-end
+  def load
+  cvs_options = {col_sep: ',', quote_char: '"'}
+  CSV.foreach(@file_path) do |row|
+    @all_recipes << row
+  end
+
+
+  def save
+  CSV.open(@file_path, 'w') do |csv|
+    csv << all_recipes
+  end
+
+  def add_recipe_to_csv(recipe)
+    @all_recipes << recipe
+    save
+  end
+
+  def remove_recipe_from_csv(recipe)
+     @all_recipes = @recipes.delete(recipe)
+     csv << [@all_recipes]
+     save
+  end
+
+
