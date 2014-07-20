11f13215828d76b9d8157c909c46ce4de3f2a286
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-20 15:40:12 +0200

    solved

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -1,27 +1,27 @@
 class Animal
-  # Modify the following methods to enable chaining !
+
   def name(name)
     @name = name
-    #TODO: find good instruction
+    self
   end
-  
+
   def specie(specie)
     @specie = specie
-    #TODO: find good instruction
+    self
   end
-  
+
   def color(color)
     @color = color
-    #TODO: find good instruction
+    self
   end
-  
+
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
-    #TODO: find good instruction
+    self
   end
-  
+
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
-  
+
 end
