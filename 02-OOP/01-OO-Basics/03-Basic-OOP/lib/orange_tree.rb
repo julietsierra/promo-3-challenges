5f461d6e00efd0ef107913c6b5772cee80d89652
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-20 22:37:20 +0200

    solved

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,51 +1,47 @@
 class OrangeTree
 
-  attr_reader :height,:fruit, :age
+  attr_reader :height,:fruits,:age
 
   def initialize
     @height = 0
     @age = 0
-    @fruit = 0
-    @dead = false
+    @fruits = 0
   # TODO: Implement all the specs defined in the README.md :)
   end
 
-
-
- def growing_fruits
-    if @age <= 5
-      @fruit = 0
-    elsif @age > 5 && @age <= 10
-      @fruit =  100
-    elsif @age > 10 && @age< 15
-          @fruit = 200
-    else @fruit = 0
-    end
-  end
-
-  def found_dead?
-    false
-    if @age >= 100
-   return true
-    end
-  end
-
   def one_year_passes!
-     @age += 1
-     if age <= 10
-      @height +=1
+   if @age < 100
+    @age += 1
+    end
+    if @age <= 10
+         @height += 1
+    end
+    if @age > 100
+      return true
+     end
+    fruit
+  end
 
-    growing_fruits
-    found_dead?
+   def dead?
+    (51..100).to_a.sample < @age
   end
 
 
 
+ def fruit
+    if (0..5).cover?(@age)
+      @fruits = 0
+    elsif (6..10).cover?(@age)
+      @fruits = 100
+    elsif (11..15).cover?(@age)
+      @fruits = 200
+    else
+      @fruits = 0
+    end
+end
 
 
-
-
- def pick_a_fruit?
+ def pick_a_fruit!
    @fruits -= 1
  end
 
@@ -53,29 +49,4 @@ end
 
 
 
-
-
-
-#if age < 50
-   #   probability_of_dying = 0
-  #  else age > 50
-    #  probability_of_dying = probability_of_dying +1
-   # end
-
-#def height(meter,age)
-   # meter=0
-   # age=0
-    #while age < 10
-      # meter = meter + 1
-    #end
-#end
-
-#def age(year)
- # year = 0
-  #Until year = 100
-   # year = year +1
-#end
-
-#def fruits_produced(year,fruits)
-  #while year <= 5
 
