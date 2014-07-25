a7d056e4caebe79c8657239acc15f863f82d1b8d
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-25 12:10:08 +0200

    ok

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -1,32 +1,47 @@
 require 'sqlite3'
 
+
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{ CREATE TABLE IF NOT EXISTS posts
+   (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name VARCHAR (100),
+    source_url VARCHAR (300),
+    date DATETIME,
+    rating INT
+  )
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
 end
 
 def create_post(db, post)
-  #TODO: add a new post to your db
+  db.execute "INSERT INTO posts (name,rating,source_url,date)
+  VALUES ('#{post[:name]}',#{post[:rating]},'#{post[:source_url]}','#{post[:date]}')"
+
 end
 
 def get_posts(db)
-   #TODO: list all posts
+stm = db.execute "SELECT * FROM posts"
+stm.flatten
+
 end
 
 def get_post(db, id)
+ puts db.execute "SELECT * FROM posts WHERE id = #{id}"
+
   #TODO: get a specific post
 end
 
 def update_post(db, id, name)
+puts db.execute "UPDATE post SET name = 'name' WHERE id = id"
 	#TODO: update a post's name in your db
 end
 
 def delete_posts(db)
-  #TODO: delete a post in your db
+ db.execute "DELETE FROM posts"
 end
 
 def delete_post(db, id)
-  #TODO: delete a specific post in your db
+  db.execute "DELETE FROM post WHERE id = #{id}"
 end
