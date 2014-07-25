e9d25672bf3280bb604762b6c903f3c0cc08dad8
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-25 17:08:29 +0200

    so so

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -6,19 +6,22 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   tab = db.execute "COUNT * FROM #{table_name}"
-  tab.flatten
 end
 
 def sorted_artists(db)
+  tab = []
   tab = db.execute "SELECT name FROM artist ORDER by name DESC "
   tab.flatten
 end
 
 def love_tracks(db)
- tab = db.execute "SELECT * FROM track WHEN name '%(love)%'"
+ tab = db.execute "SELECT name FROM track WHEN name '%(love)%'"
  tab.flatten
 end
 
 def long_tracks(db, min_length)
+  time = min_length * 60000
+  array = db.execute "SELECT name FROM track WHERE milliseconds > #{time} ORDER BY milliseconds DESC"
+  array.flatten
   #TODO: return tracks verifying: duration > min_length (minutes)
 end
