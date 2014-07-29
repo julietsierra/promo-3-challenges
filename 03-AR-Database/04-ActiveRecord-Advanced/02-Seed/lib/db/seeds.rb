6c00a539491cec70940a62cfa42f0d9689cd6e38
Juliette Sierra <sanson.juliette@gmail.com>
2014-07-29 18:49:59 +0200

    mes exos de la journée

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
@@ -6,8 +6,24 @@ require 'faker'
 require 'nokogiri'
 require 'open-uri'
 
-puts 'Seeding database...'
+user = User.create(
+  name:                   Faker::Name.name,
+  email:                  Faker::Internet.email,
+  password:               password,
+  password_confirmation:  password)
+
+
+data_origin = ENV['from'] || 'local file'
+data_url = 'http://news.ycombinator.com/'
+
+puts 'Seeding data for Hackernews based on #{data_url}.'
 
 # TODO: Your code goes here
 # 1. create fake users
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+
+
+
+puts 'Removing data...'
+Post.delete_all
+User.delete_all
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/interface.rb
@@ -3,3 +3,47 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 # TODO: build your interface with login and CRUD tasks on the posts
+def ask_and_get(param)
+  puts "What is the #{param} of your piece of news?"
+  gets.chomp
+end
+
+while true
+
+  logged_in = false
+
+  begin
+    puts "Please login with your <id>"
+    id = gets.chomp.to_i
+    begin
+      user = User.find(id)
+    rescue
+      puts "login error"
+    end until user
+  end
+
+  puts "Hey #{user_name} what do you want to do?"
+  puts "1 Create a post"
+  puts "2 Read posts"
+  puts "3 Update a post"
+  puts "4 Delete a post"
+  puts "5 to exit program"
+
+
+  choice = gets.chomp.to_i
+
+  case choice
+  when 1
+    name = ask_and_get("name")
+    source_url = ask_and_get("source_url")
+    rating = ask_and_get("rating")
+    post = {name: name, source_url: source_url, date: Time.new rating: rating }
+    user.posts.create(post)
+  when 2
+   user.posts.each do |post|
+    puts "#{post_id}, #{post.name}, #{post.date}, #{post.source_url}, #{post.rating}"
+
+
+
+
+
